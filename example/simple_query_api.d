01d1b53a53cc2cbfe1e6c01f7eac7a8b9e67fae9
Masahiro Nakagawa <repeatedly@gmail.com>
2012-05-06 08:58:14 +0900

    Add simple query api test to example

---
+++ example/simple_query_api.d
@@ -0,0 +1,112 @@
+import avocado.collection;
+import avocado.database;
+
+import std.array;
+import std.algorithm;
+import std.json;
+import std.range;
+import std.stdio;
+import std.traits;
+import std.typecons;
+
+void cleanupCollections()
+{
+    auto database = new Database(Configuration());
+    foreach (collection; database.collections)
+        database.deleteCollection(collection.name);
+}
+
+void main()
+{
+    cleanupCollections();
+
+    auto database = new Database(Configuration());
+    auto collection = database.createCollection(Database.CollectionProperty("sample_collection"));
+
+    void putDocuments()
+    {
+        import std.conv;
+
+        foreach (i; 0..100)
+            collection.putDocument(["class": "a", "id": i.to!string()]);
+        foreach (i; 0..100)
+            collection.putDocument(["class": "b", "id": i.to!string()]);
+        foreach (i; 0..100)
+            collection.putDocument(["class": "c", "id": i.to!string()]);
+    }
+
+    writeln("Put documents");
+    putDocuments();
+
+    {
+        writeln("simple/all");
+        auto cursor = collection.queryAll();
+        {
+            size_t i = 0;
+            foreach (doc; cursor) i++;
+            assert(i == 300);
+        }
+
+        writeln("simple/all with 250 skip");
+        AllOption option;
+        option.skip = 250;
+        auto skippedCursor = collection.queryAll(option);
+        {
+            size_t i = 0;
+            foreach (doc; skippedCursor) i++;
+            assert(i == 50);
+        }
+
+        writeln("simple/all with 250 skip and 10 limit");
+        option.limit = 10;
+        auto limitedCursor = collection.queryAll!(string[string])(option);
+        {
+            size_t i = 0;
+            foreach (doc; limitedCursor) i++;
+            assert(i == 10);
+        }
+    }
+
+    {
+        immutable classes = ["a", "b", "c"];
+
+        writeln("simple/by-example for class");
+        foreach (cls; classes) {
+            auto result = collection.queryByExample(["class": cls]);
+            assert(result.length == 100);
+        }
+
+        writeln("simple/by-example for each id");
+        foreach (id; 0..100) {
+            auto result = collection.queryByExample!(string[string])(["id": id.to!string()]);
+            assert(result.length == 3);
+            foreach (doc; result)
+                assert(!find(classes, doc["class"]).empty);
+        }
+
+        writeln("simple/by-example with 90 skip");
+
+        ByExampleOption option;
+        option.skip = 90;
+        foreach (cls; classes) {
+            auto result = collection.queryByExample(["class": cls], option);
+            assert(result.length == 10);
+        }
+
+        writeln("simple/by-example with 50 skip and 10 limit");
+        option.skip = 50;
+        option.limit = 20;
+        foreach (cls; classes) {
+            auto result = collection.queryByExample(["class": cls], option);
+            assert(result.length == 20);
+        }
+    }
+
+    //ByExampleOption option;
+    //option.skip = 10;
+    //writeln(collection.queryByExample!(string[string])(["name": "shinobu"], option));
+    //writeln(collection.queryByExample!(string[string])(["name": "shinobu"]));
+    //writeln(database.putDocument(["c": true], "test"));
+
+    cleanupCollections();
+}
